ca4c17dd83fea9dfe758f2eb59301111d0476c38
Farnaz Farzipour <farnaziiifz@Farnazs-MacBook-Air.local>
2023-11-13 15:48:30 +0330

    fix columns

--- src/presentation/pages/Client/ClientDetails.vue
+++ src/presentation/pages/Client/ClientDetails.vue
@@ -1,61 +1,13 @@
 <script lang="ts" setup>
+
 import ContentLayout from '../../layouts/ContentLayout.vue'
-import {
-  clientDetailList,
-  clientDetail,
-} from '../../../core/types/ClientDetail.type'
-import {
-  clientDetailOrder,
-  clientDetailListOrder,
-} from '../../../core/types/Clientdetailorder.type'
-import { TableColumnType } from 'ant-design-vue'
-import { ref, Ref } from 'vue'
+import { ref } from 'vue'
+import Wallet from './components/wallet.vue'
+import Orders from './components/orders.vue'
+
 const activeKey = ref('1')
 const showStatusModal = ref(false)
 const itemForChangeStatus = ref()
-const datacol: Ref<clientDetailList> = ref({
-  data: [
-    { id: 1, chargingwallet: 'توسط کاربر', count: 554 },
-    { id: 2, chargingwallet: 'کش بک', count: 54 },
-    { id: 3, chargingwallet: 'هدیه اعتباری', count: 55 },
-    { id: 4, chargingwallet: 'کوپن', count: 55 },
-  ],
-})
-const datacolorder: Ref<clientDetailListOrder> = ref({
-  data: [
-    { orderNumber: 'توسط کاربر', dateoforder: 'kinnin', amountmoney: 1565165 },
-  ],
-})
-const columnorder: TableColumnType<clientDetailOrder>[] = [
-  {
-    title: 'شماره سفارش',
-    key: 'orderNumber',
-    dataIndex: 'orderNumber',
-  },
-  {
-    title: 'تاریخ ثبت سفارش',
-    key: 'dateoforder',
-    dataIndex: 'dateoforder',
-  },
-  {
-    title: 'مبلغ پرداختی',
-    key: 'amountmoney',
-    dataIndex: 'amountmoney',
-  },
-]
-
-const columns: TableColumnType<clientDetail>[] = [
-  {
-    title: 'نحوه ی شارژ کیف پول',
-    key: 'chargingwallet',
-    dataIndex: 'chargingwallet',
-  },
-  {
-    title: 'مقدار',
-    key: 'count',
-    dataIndex: 'count',
-  },
-]
 
 const changestatus = (status: boolean, id: number) => {
   showStatusModal.value = !showStatusModal.value
@@ -85,27 +37,10 @@ const changestatus = (status: boolean, id: number) => {
           <div></div>
         </a-tab-pane>
         <a-tab-pane key="2" tab="کیف پول" force-render>
-          <div>
-            <a-typography-title :level="4" class="mr-2 mb-1"
-              >کیف پول</a-typography-title
-            >
-          </div>
-          <div>
-            <a-typography-text disabled>موجودی</a-typography-text>
-          </div>
-          <div>/*async</div>
-          <a-table :columns="columns" :data-source="datacol.data"> </a-table>
+          <Wallet />
         </a-tab-pane>
         <a-tab-pane key="3" tab="سفارش ها">
-          <div>
-            <a-typography-title :level="4" class="mr-2 mb-1"
-              >لیست سفارش ها</a-typography-title
-            >
-          </div>
-          <a-table
-            :columnorder="columnorder"
-            :data-source="datacolorder.data"
-          ></a-table>
+          <Orders />
         </a-tab-pane>
         <a-tab-pane key="4" tab="پرداخت ها">Content of Tab Pane 3</a-tab-pane>
         <a-tab-pane key="5" tab="مشوق ها">Content of Tab Pane 3</a-tab-pane>
